57df712a5f38838ea531e3eee6a97d9ced9fa55d
thecsharper <si@thecsharper.com>
2023-10-31 13:34:24 +0000

    Add project files.

---
+++ ResultPatternDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+
+var successResult1 = Result.Ok();
+
+// create a result which indicates failure
+var errorResult1 = Result.Fail("My error message");
+var errorResult2 = Result.Fail(new Error("My error message"));
+var errorResult4 = Result.Fail(new List<string> { "Error 1", "Error 2" });
+var errorResult5 = Result.Fail(new List<IError> { new Error("Error 1"), new Error("Error 2") });
